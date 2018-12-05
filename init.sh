1e09bc7aa3a25a218c7b2273a62b87e3964420bf
Jim Robinson <jim@croud.co.uk>
2018-12-05 10:28:49 +0000

    Initial

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+KUBECONFIG=$PWD/kubeconfig-test-mrwolf
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+watch --color -n1 "curl --fail -m2 \$(kubectl get service goldengoose -n default --kubeconfig $KUBECONFIG -o json | jq .status.loadBalancer.ingress | jq -r \".[0].ip\") >/dev/null 2>&1 && echo \"Group mrwolf - ${GREEN}goldengoose is UP${NC}\" || echo \"Group mrwolf - ${RED}goldengoose is DOWN${NC}\"; kubectl get deployment goldengoose -n default --kubeconfig $KUBECONFIG; kubectl get pods -l app=goldengoose,track=prod --kubeconfig $KUBECONFIG"
